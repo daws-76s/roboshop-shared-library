6ceb322adb5ca08fa48b6ee12d982f7e49b43577
joindevopscloud <info@joindevops.com>
2024-02-05 08:50:02 +0530

    jenkins

---
+++ vars/pipelineDecission.groovy
@@ -0,0 +1,19 @@
+#!groovy
+
+def decidePipeline(Map configMap){
+    application = configMap.get("application")
+    switch(application) {
+        case 'nodejsVM'
+            nodejsVM(configMap)
+            break
+        case 'javaVM'
+            javaVM(configMap)
+            break
+        case 'nodejsEKS'
+            nodejsEKS(configMap)
+            break
+        default:
+            error "Application is not recognised"
+            break
+    }
+}
