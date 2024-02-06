fefd061ad14401fb7f42b0b323b9268373e8725d
joindevopscloud <info@joindevops.com>
2024-02-06 08:00:54 +0530

    jenkins

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -89,7 +89,7 @@ def call(Map configMap){
             stage('Deploy') {
                 when {
                     expression{
-                        params.Deploy is 'true'
+                        params.Deploy
                     }
                 }
                 steps {
