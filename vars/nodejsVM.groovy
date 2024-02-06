1a3958e9e843400be8d3ce6a75b668215ca0ac54
joindevopscloud <info@joindevops.com>
2024-02-06 07:46:55 +0530

    jenkins

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -53,7 +53,8 @@ def call(Map configMap){
             stage('Sonar Scan'){
                 steps{
                     sh """
-                        sonar-scanner
+                        #sonar-scanner
+                        echo "sonar scan will run here"
                     """
                 }
             }
